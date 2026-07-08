618542eb736ff577c2d591685c9eb11513f382d0
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-09 03:49:34 +0530

    Worked on function which dont have return a value.

--- index.ts
+++ index.ts
@@ -1,26 +1,11 @@
-function addTwo(num: number): number {
-  // num.toUpperCase(); //not allow
-  return num + 2;
-  //return "hello";
+//Some function never return a value
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+  //return 1;
 }
 
-// function getValue(val: number): string {
-//   if (val > 5) {
-//     return true; //error
-//   }
-//   return "200 Ok";
-// }
+function handleError(errmsg: string): void {
+  throw new Error(errmsg);
+}
 
-const getHello = (s: string): string => {
-  return "";
-};
-
-const heros = ["thor", "Spiderman", "isronman"];
-//const heros = [1, 3, 6];
-
-heros.map((hero): string => {
-  return `hero is ${hero}`;
-  //return 1;
-});
-
-console.log("mottuu");
+export {};
