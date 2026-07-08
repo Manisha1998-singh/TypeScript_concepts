e917e6ef3a1aaa7d1c52a434f6583c8e08a95066
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-09 04:06:25 +0530

    bad behavior of objects.

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
-//Some function never return a value
-function consoleError(errmsg: string): void {
-  console.log(errmsg);
-  //return 1;
-}
-
-function handleError(errmsg: string): never {
-  throw new Error(errmsg);
+const User = {
+  name: "MAnisha",
+  email: "mani@gamil.com",
+  isActive: true,
+};
+function createUser({ name, isPaid }: { name: string; isPaid: boolean }) {}
+let newUser = { name: "tushar", isPaid: false, email: "m@h.com" };
+createUser(newUser);
+createUser({ name: "MAnisha", isPaid: false });
+function cerateCourse(): { name: string; price: number } {
+  return { name: "tushar", price: 3 };
 }
 
 export {};
