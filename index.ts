8626f12ffe911000b1bd7c3973507dcc0a19f67c
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-09 02:39:49 +0530

    worked on variables.

--- index.ts
+++ index.ts
@@ -2,3 +2,14 @@ let message: string = "Hello TypeScript";
 console.log(message);
 //message = 10; // provide safety
 console.log(message);
+
+//Number
+let UserId = 3336.7;
+//UserId= "mannu" auto show error
+
+UserId.toFixed();
+
+// boolean
+let isLogin: boolean = false;
+
+//
