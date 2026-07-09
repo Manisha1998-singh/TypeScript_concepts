16c8c7f6a9ae9ab60838b1f74b90a5793a91c5d4
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-09 05:37:09 +0530

    Worked on readonly, optional and combine.

--- index.ts
+++ index.ts
@@ -1,11 +1,30 @@
 type User = {
+  readonly _id: string;
   name: string;
   email: string;
   isActive: boolean;
+  cardDetails?: number; //optional
 };
 
-function createUser(user: User) {
-  return { name: "", email: "", isActive: true };
-}
-createUser({ name: "", email: "", isActive: true });
-export {};
+let myUser: User = {
+  _id: "1234",
+  name: "m",
+  email: "m@gmail.com",
+  isActive: false,
+};
+
+type cardNumber = {
+  cardnumber: string;
+};
+type cardDate = {
+  cardDate: string;
+};
+
+type cardDetails = cardNumber &
+  cardDate & {
+    cvv: number;
+  }; // Use for combine
+
+myUser.email = "hm@gmail.com";
+//myUser._id = "asa"; // not allowed
+console.log(myUser.email);
