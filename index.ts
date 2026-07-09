22925db5ba5f3f5b7d24807122010ef66be7e53a
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-09 05:52:45 +0530

    Array

--- index.ts
+++ index.ts
@@ -1,30 +1,20 @@
+//Array way -1
+
+const superArray: string[] = [];
+
+//Array way - 2
+
+const childArray: Array<number> = [];
+
 type User = {
-  readonly _id: string;
   name: string;
-  email: string;
   isActive: boolean;
-  cardDetails?: number; //optional
 };
+const allUsers: User[] = [];
+allUsers.push({ name: "mannu", isActive: true });
 
-let myUser: User = {
-  _id: "1234",
-  name: "m",
-  email: "m@gmail.com",
-  isActive: false,
-};
-
-type cardNumber = {
-  cardnumber: string;
-};
-type cardDate = {
-  cardDate: string;
-};
-
-type cardDetails = cardNumber &
-  cardDate & {
-    cvv: number;
-  }; // Use for combine
-
-myUser.email = "hm@gmail.com";
-//myUser._id = "asa"; // not allowed
-console.log(myUser.email);
+console.log(superArray.push("Spiderman"));
+console.log(childArray.push(5));
+console.log(childArray);
+console.log(superArray);
+console.log(allUsers);
