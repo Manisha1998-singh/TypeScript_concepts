266cfc26df27c8633360f5febea04f4d08b520a0
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-11 02:20:03 +0530

    Worked on enums

--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
-//Tuples
+//Enums
 
-let tUser: [string, number, boolean];
-//tUser = [ 7, "4",  6, true]; //error
-tUser = ["4", 6, true]; //error enter the same data type order
-console.log(tUser);
-//let newUser: tUser = ["5", 6, 9]; //not allowed
-type nUser = [string, number, boolean];
-const newUser: nUser = ["5", 6, true];
-newUser[0] = "mannu"; // allowed
-newUser.push(true); //allow
-console.log(newUser);
+enum SeatChoice {
+  AISLE = 10,
+  WINDOW,
+  MIDDlE,
+}
+const hcSeat = SeatChoice.MIDDlE;
+console.log(hcSeat);
