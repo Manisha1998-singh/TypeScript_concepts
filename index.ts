e536fd09c9fbd578dee76bf1e3217892dfbdfdc3
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-10 18:18:34 +0530

    Worked on Tuples.

--- index.ts
+++ index.ts
@@ -1,36 +1,6 @@
-//Union
+//Tuple
 
-let score: number | string = 33;
-score = 44;
-score = "55";
-
-type User = {
-  name: string;
-  id: number;
-};
-type Admin = {
-  name: string;
-  id: number;
-};
-
-let mannu: User | Admin = { name: "Tisha", id: 33 };
-
-mannu = { name: "hc", id: 335 };
-
-function getDbId(id: string | number) {
-  if (typeof id === "string") {
-    console.log(id.toLowerCase());
-  }
-}
-getDbId("manii");
-
-//both type array
-
-const data: number[] = [1, 2, 4, 5];
-const data1: string[] = ["1", "4", "5"];
-const data3: (string | number | boolean)[] = ["1", "4", "5", 6, true];
-console.log(data3);
-
-let seatAllotment: "aisle" | "middle" | "window";
-seatAllotment = "aisle";
-//seatAllotment = "crew"; fixed value not allowed
+let tUser: [string, number, boolean];
+//tUser = [ 7, "4",  6, true]; //error
+tUser = ["4", 6, true]; //error enter the same data type order
+console.log(tUser);
