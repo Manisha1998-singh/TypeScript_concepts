6ad53cb5609caf80bc164a0e2d30f606521bd334
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-10 17:56:04 +0530

    Stop tracking index.js.map

--- index.ts
+++ index.ts
@@ -1,20 +1,36 @@
-//Array way -1
+//Union
 
-const superArray: string[] = [];
-
-//Array way - 2
-
-const childArray: Array<number> = [];
+let score: number | string = 33;
+score = 44;
+score = "55";
 
 type User = {
   name: string;
-  isActive: boolean;
+  id: number;
 };
-const allUsers: User[] = [];
-allUsers.push({ name: "mannu", isActive: true });
+type Admin = {
+  name: string;
+  id: number;
+};
 
-console.log(superArray.push("Spiderman"));
-console.log(childArray.push(5));
-console.log(childArray);
-console.log(superArray);
-console.log(allUsers);
+let mannu: User | Admin = { name: "Tisha", id: 33 };
+
+mannu = { name: "hc", id: 335 };
+
+function getDbId(id: string | number) {
+  if (typeof id === "string") {
+    console.log(id.toLowerCase());
+  }
+}
+getDbId("manii");
+
+//both type array
+
+const data: number[] = [1, 2, 4, 5];
+const data1: string[] = ["1", "4", "5"];
+const data3: (string | number | boolean)[] = ["1", "4", "5", 6, true];
+console.log(data3);
+
+let seatAllotment: "aisle" | "middle" | "window";
+seatAllotment = "aisle";
+//seatAllotment = "crew"; fixed value not allowed
