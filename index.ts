ff16a8902b357169c7a57f34cda4c7422050e0d8
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-10 23:41:15 +0530

    Worked on Tuples.

--- index.ts
+++ index.ts
@@ -1,6 +1,12 @@
-//Tuple
+//Tuples
 
 let tUser: [string, number, boolean];
 //tUser = [ 7, "4",  6, true]; //error
 tUser = ["4", 6, true]; //error enter the same data type order
 console.log(tUser);
+//let newUser: tUser = ["5", 6, 9]; //not allowed
+type nUser = [string, number, boolean];
+const newUser: nUser = ["5", 6, true];
+newUser[0] = "mannu"; // allowed
+newUser.push(true); //allow
+console.log(newUser);
