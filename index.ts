578f0dcd021489874d4b130190f5d2ed106dba35
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-11 03:20:27 +0530

    Worked on classes.

--- index.ts
+++ index.ts
@@ -1,44 +1,13 @@
-//Interface
-
-interface User {
-  readonly dbId: number;
+//Class
+class User {
   email: string;
-  userId: number;
-  googleId: string;
-  //startTrail: () => string
-  startTrail(): string;
-  getCoupon(couponname: string, value: number): number;
+  name: string;
+  readonly city: string = "jaipur";
+  constructor(email: string, name: string) {
+    this.email = email;
+    this.name = name;
+  }
 }
 
-const manisha: User = {
-  dbId: 22,
-  email: "h@h.com",
-  userId: 2211,
-  googleId: "h@h.com",
-  startTrail: () => {
-    return "trail started";
-  },
-  getCoupon: (name: "manisha") => {
-    return 10;
-  },
-};
-manisha.email = "h@h.com";
-interface USer {
-  githubToken: string;
-}
-interface Admin extends User {
-  role: "admin" | "ta" | "learner";
-}
-const manisha1: Admin = {
-  role: "admin",
-  dbId: 22,
-  email: "h@h.com",
-  userId: 2211,
-  googleId: "h@h.com",
-  startTrail: () => {
-    return "trail started";
-  },
-  getCoupon: (name: "manisha") => {
-    return 10;
-  },
-};
+const manisha = new User("h@h.com", "mannu");
+//manisha.city = "jaipur"; // error it readonly
