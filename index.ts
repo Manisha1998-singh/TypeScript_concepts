d64d2a8656e8992673aabf4921698c1b1acf30b5
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-11 02:44:37 +0530

    Worked on interface and extends.

--- index.ts
+++ index.ts
@@ -23,3 +23,22 @@ const manisha: User = {
   },
 };
 manisha.email = "h@h.com";
+interface USer {
+  githubToken: string;
+}
+interface Admin extends User {
+  role: "admin" | "ta" | "learner";
+}
+const manisha1: Admin = {
+  role: "admin",
+  dbId: 22,
+  email: "h@h.com",
+  userId: 2211,
+  googleId: "h@h.com",
+  startTrail: () => {
+    return "trail started";
+  },
+  getCoupon: (name: "manisha") => {
+    return 10;
+  },
+};
