f67c6003d76e68c4e62c4bb191493a6350f7b7bd
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-11 03:28:07 +0530

    Worked on private and public.

--- index.ts
+++ index.ts
@@ -1,11 +1,23 @@
 //Class
+// class User {
+//   public email: string;
+//   private name: string;
+//   readonly city: string = "jaipur";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
   readonly city: string = "jaipur";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
+  constructor(
+    public email: string,
+    public name: string,
+    //private userId: string,
+  ) {
+    //this.email = email;
+    //this.name = name;
   }
 }
 
