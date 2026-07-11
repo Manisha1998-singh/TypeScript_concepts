6c6500559337e16ba0086ff39ce6562566f1df31
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-11 09:34:14 +0530

    Worked on INerface .

--- index.ts
+++ index.ts
@@ -1,36 +1,31 @@
-//Getters and setters
-class User {
-  protected _courseCount = 1;
-  readonly city: string = "jaipur";
+//Interface
+
+interface TakePhoto {
+  cameraMode: string;
+  filter: string;
+  burst: number;
+}
+interface Story {
+  createStory(): void;
+}
+
+class Instagram implements TakePhoto {
   constructor(
-    public email: string,
-    public name: string,
-    //private userId: string,
+    public cameraMode: string,
+    public filter: string,
+    public burst: number,
+  ) {}
+}
+
+class YouTube implements TakePhoto, Story {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number,
+    public short: string,
   ) {}
 
-  private deleteToken() {
-    console.log("Token deleted");
-  }
-
-  get getAppleEmail(): string {
-    return `apple${this.email}`;
-  }
-  get courseCount(): number {
-    return this._courseCount;
-  }
-  set courseCount(courseNum) {
-    if (courseNum <= 1) {
-      throw new Error("check error ");
-    }
-    this._courseCount = courseNum;
+  createStory(): void {
+    console.log("Story was created");
   }
 }
-
-class SubUser extends User {
-  isFamily: boolean = true;
-  changeCourseCount() {
-    this._courseCount = 4;
-  }
-}
-const manisha = new User("h@h.com", "mannu");
-//manisha.city = "jaipur"; // error it readonly
