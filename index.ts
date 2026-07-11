8145a60cba6eb9cdc3246c447608bdd30c100187
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-11 17:12:16 +0530

    Worked on Gentics.

--- index.ts
+++ index.ts
@@ -1,29 +1,26 @@
-//abstract
-abstract class TakePhoto {
-  constructor(
-    public cameraMode: string,
-    public filter: string,
-    public burst: number,
-  ) {}
-  abstract getSepia(): void;
-  getRealTime(): number {
-    //some complex calculation
-    return 8;
-  }
+//Genrics
+
+const score: Array<number> = [];
+const names: Array<string> = [];
+
+function identityOne(val: boolean | number): boolean | number {
+  return val;
 }
 
-class Instagram extends TakePhoto {
-  constructor(
-    public cameraMode: string,
-    public filter: string,
-    public burst: number,
-  ) {
-    super(cameraMode, filter, burst);
-  }
-  getSepia(): void {
-    console.log("sepia");
-  }
+function identityTwo(val: any): any {
+  return val;
 }
 
-const ms = new Instagram("test", "Test", 9);
-ms.getRealTime();
+function identityThree<Type>(val: Type): Type {
+  return val;
+}
+identityThree(5);
+function identityFive<T>(val: T): T {
+  return val;
+}
+interface Bootle {
+  brand: string;
+  types: number;
+}
+identityFive<Bootle>({ brand: "Tum", types: 5 });
+console.log(identityFive(5));
