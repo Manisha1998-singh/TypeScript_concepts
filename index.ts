1e1b8db8b664d67d61585c9e958ce81c97dd0451
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-11 09:52:44 +0530

    Worked on abstract classes.

--- index.ts
+++ index.ts
@@ -1,31 +1,29 @@
-//Interface
-
-interface TakePhoto {
-  cameraMode: string;
-  filter: string;
-  burst: number;
-}
-interface Story {
-  createStory(): void;
-}
-
-class Instagram implements TakePhoto {
+//abstract
+abstract class TakePhoto {
   constructor(
     public cameraMode: string,
     public filter: string,
     public burst: number,
   ) {}
-}
-
-class YouTube implements TakePhoto, Story {
-  constructor(
-    public cameraMode: string,
-    public filter: string,
-    public burst: number,
-    public short: string,
-  ) {}
-
-  createStory(): void {
-    console.log("Story was created");
+  abstract getSepia(): void;
+  getRealTime(): number {
+    //some complex calculation
+    return 8;
   }
 }
+
+class Instagram extends TakePhoto {
+  constructor(
+    public cameraMode: string,
+    public filter: string,
+    public burst: number,
+  ) {
+    super(cameraMode, filter, burst);
+  }
+  getSepia(): void {
+    console.log("sepia");
+  }
+}
+
+const ms = new Instagram("test", "Test", 9);
+ms.getRealTime();
