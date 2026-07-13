7a41fe3c188055de0250918efa3428e9667f7ee1
Manisha Singh <manishasinghbca89@gmail.com>
2026-07-14 04:04:37 +0530

    Narrowing.

--- index.ts
+++ index.ts
@@ -4,19 +4,23 @@ interface User {
   name: string;
   email: string;
 }
-interface Admin {
-  name: string;
-  email: string;
-  isAdmin: boolean;
+
+//instanceof work with classes  and not work with same name interface
+class Admin {
+  constructor(
+    public name: string,
+    public email: string,
+    public isAdmin: boolean,
+  ) {}
 }
-function isAdminAccount(account: User | Admin) {
-  if ("isAdmin" in account) {
-    return account.isAdmin;
-  }
+const manisha = new Admin("Manisha", "abc@gmail.com", true);
+
+if (manisha instanceof Admin) {
+  console.log(manisha.name.toUpperCase());
 }
+
 const newUser: Admin = {
   name: "Manisha",
   email: "manisha@gmail.com",
   isAdmin: true,
 };
-console.log(isAdminAccount(newUser));
